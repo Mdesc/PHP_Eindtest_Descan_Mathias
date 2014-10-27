01802c046f3eb3a7888e398f7ff6fa9e3126645f
lok117 <lok117@LOK117-01>
2014-10-27 16:27:37 +0100

    hmm

--- index.php
+++ index.php
@@ -15,3 +15,572 @@ and open the template in the editor.
         ?>
     </body>
 </html>
+
+materials needed
+7       azerothian diamond          
+5       aquamarine
+4       alicite
+125     adamanite bar
+15      abyss crystal 
+340     arcane dust
+16      azshara's veil 
+25      adder's tongue 
+365     bronze bar
+3       blue sapphire 
+1       black vitriol 
+10      blindweed 
+33      bruiseweed 
+93      briathorn 
+320     Borean Leather
+25      citrine
+385     copper bars
+608     cobalt bar
+100     coarse stone 
+20      crystalized air
+124     crystallized water
+10      crystallized earth 
+15      crystal vial 
+1       copper rod
+60      cinderbloom 
+60      dense stone 
+3       dream chard 
+230     dream dust 
+35      dreaming glory 
+15      dreamfoil 
+3       essence of the undeadth
+160     elementium bar 
+300     Exotic Leather or Prismatic Scale
+40      Exotic Leather
+5       Eternal Water
+905     Embersilk Cloth
+20      Eternal Life or Fire or Shadow
+102     fel iron bar
+25      fool's cap 
+50      felweed 
+4       firebloom 
+26      Fel Scales
+1208    Frostweave Cloth
+618     ghost iron bar 
+30      greater celestial essence 
+27      greater cosmic essence 
+10      greater magic essence 
+100     grean tea leaf 
+86      goldclover
+46      golden sansam
+40      gromsblood
+35      goldthorn 
+20      huge emerald 
+215     heavy stone
+353     hypnotic dust 
+25      heartblossom
+21      Heavy Savage Leather (110 Savage Leather)
+120     Heavy Borean Leather (720 Borean Leather)
+20      Heavy Hide
+195     Heavy Leather
+30      Hypnotic Dust
+284     iron bar 
+122     infinite dust 
+20      icethorn
+45      khadgar's whisker 
+30      kingsblood 
+100     Knothide Leather Scraps
+317     Knothide Leather
+20      large opal 
+60      lesser moonstone
+280     linen cloth 
+10      lesser celestial essence 
+5       large prismatic shard 
+20      lesser planar essence 
+25      lesser nether essence 
+15      lesser eternal essence 
+25      lesser astral essence
+40      lichbloom 
+20      liferoot
+260     Light Leather
+621     mithril bar
+24      mote of earth 
+12      mote of fire    
+4       mos agate  
+626     mageweave cloth 
+34      mysterious essence 
+18      mountain silversage 
+20      mageroyal 
+175     Medium Leather
+15      Medium Hide
+15      nightmare vine 
+10      netherbloom 
+830     Netherweave Cloth
+241     obsidium bar 
+24      purple lotus
+59      peacebloom
+193     rough stone
+60      rain poppy or silkweed
+490     Rugged Leather
+57      Ruined Leather Scraps
+801     Runecloth
+10      star ruby 
+20      small lustrous pearl
+10      spirit of harmony
+54      steel bar 
+220     solid stone 
+172     saronite bar 
+7       silver bar 
+137     spirit dust
+120     soul dust 
+10      simple wood vendor 
+134     strange dust
+60      snow lily
+10      stormvine 
+30      sorrowmoss 
+25      sungrass
+45      stranglekelp 
+59      silverleaf
+65      Sha-Touched Leather
+50      Savage Leather Scraps
+385     Savage Leather
+620     Silk Cloth
+899     thorium bar
+15      true silver bar
+20      tigerseye
+35      tinker's kit
+5       titanium bar 
+75      tin bar 
+5       twilight jasmine 
+37      tigerlily 
+20      talandra's rose
+10      terocone
+410     Thick Leather
+78      volatile earth
+32      volatile air
+54      volatile earth 
+10      volatile fire 
+155     vision dust 
+5       volatile life
+2       Volatile Water
+10      wild steelbloom
+495     Windwool Cloth
+195     Wool Cloth
+
+
+
+
+
+
+jewel crafting      
+100     copper bars
+20      tigerseye
+120     bronze bar
+20      small lustrous pearl
+80      heavy stone
+60      lesser moonstone
+140     mithril bar
+80      solid stone
+25      citrine
+15      true silver bar
+5       aquamarine
+50      thorium bar
+10      star ruby 
+20      large opal 
+7       azerothian diamond 
+3       blue sapphire
+3       essence of the undeadth
+20      huge emerald 
+
+
+
+engeneering     35
+60      rough stone
+36      copper bar 
+20      coarse stone 
+20      linen cloth 
+95      bronze bar 
+4       mos agate 
+30      heavy stone 
+60      wool cloth 
+15      medium leather 
+4       steel bar 
+120     solid stone 
+161     mithril bar 
+20      mageweave cloth 
+40      dense stone 
+189     thorium bar 
+21      rune cloth 
+45      fel iron bar
+24      mote of earth 
+12      mote of fire 
+40      adamanite bar
+30      netherweave cloth
+252     cobalt bar
+8       frostweave cloth
+24      crystallized water
+10      crystallized earth 
+35      tinker's kit
+85      obsidium bar 
+4       alicite
+52      volatile earth
+30      volatile air
+76      elementium bar 
+20      emberisk cloth
+85      windwool cloth
+400     ghost iron bar 
+8       spirit of harmony
+
+
+blacksmithing   28
+
+133     rough stone 
+285     copper bar 
+80      coarse stone 
+7       silver bar 
+75      tin bar 
+150     bronze bar 
+105     heavy stone 
+280     iron bar 
+50      steel bar
+20      solid stone
+150     mageweave cloth 
+320     mithril bar 
+20      dense stone
+660     thorium bar
+80      rugged leather 
+57      fel iron bar
+10      netherweave cloth
+85      adamanite bar 
+320     cobalt
+172     saronite bar 
+20      crystalized air
+5       titanium bar 
+156     obsidium bar 
+84      elementium bar 
+54      volatile earth 
+8       volatile fire 
+218     ghost iron bar 
+1       spirit of harmony
+
+
+enchanting
+
+1       copper rod
+134     strange dust
+10      greater magic essence 
+10      simple wood vendor 
+25      lesser astral essence
+120     soul dust 
+155     vision dust 
+20      purple lotus 
+25      lesser nether essence 
+15      lesser eternal essence 
+230     dream dust 
+340     arcane dust
+5       large prismatic shard 
+20      lesser planar essence 
+15      nightmare vine 
+15      crystal vial 
+102     infinite dust 
+27      greater cosmic essence 
+15      abyss crystal 
+3       dream chard 
+353     hypnotic dust 
+10      lesser celestial essence 
+30      greater celestial essence 
+137     spirit dust
+34      mysterious essence 
+
+
+alchemy
+
+
+59      peacebloom
+59      silverleaf
+93      briathorn 
+20      mageroyal 
+33      bruiseweed 
+45      stranglekelp 
+20      liferoot
+30      kingsblood 
+35      goldthorn 
+10      wild steelbloom
+25      sungrass
+45      khadgar's whisker 
+10      blindweed 
+4       iron bar 
+1       black vitriol 
+4       purple lotus 
+4       firebloom 
+40      gromsblood
+30      sorrowmoss 
+15      dreamfoil 
+46      golden sansam
+18      mountain silversage 
+50      felweed 
+10      terocone
+35      dreaming glory 
+10      netherbloom 
+20      talandra's rose
+86      goldclover
+37      tigerlily 
+25      adder's tongue 
+20      icethorn 
+40      lichbloom 
+60      cinderbloom 
+10      stormvine 
+16      azshara's veil 
+25      heartblossom
+5       volatile life
+5       twilight jasmine 
+100     grean tea leaf 
+60      rain poppy or silkweed
+60      snow lily
+25      fool's cap 
+           
+
+
+leatherworking 
+
+
+57      Ruined Leather Scraps
+15      Medium Hide
+260     Light Leather
+160     Medium Leather
+20      Heavy Hide
+195     Heavy Leather
+410     Thick Leather
+410     Rugged Leather
+100     Knothide Leather Scraps
+297     Knothide Leather
+26      Fel Scales
+320     Borean Leather
+120     Heavy Borean Leather (720 Borean Leather)
+100     Crystallized Water (10 Eternal Water)
+5       Eternal Water
+50      Savage Leather Scraps
+385     Savage Leather
+21      Heavy Savage Leather (110 Savage Leather)
+2       Volatile Fire
+2       Volatile Air
+2       Volatile Water
+26      Volatile Earth
+65      Sha-Touched Leather
+40      Exotic Leather
+300     Exotic Leather or Prismatic Scale. (Choose the cheaper one. You can also use 150 Exotic Leather and 150 Prismatic Scale for example)
+1       Spirit of Harmony
+
+
+tailoring
+
+260     Linen Cloth
+135     Wool Cloth
+620     Silk Cloth
+456     Mageweave Cloth
+780     Runecloth
+790     Netherweave Cloth
+20      Knothide Leather
+20      Infinite Dust
+20      Eternal Life or Fire or Shadow
+1200    Frostweave Cloth
+885     Embersilk Cloth
+30      Hypnotic Dust
+410     Windwool Cloth
+
+
+inscription 
+
+140     Alabaster Pigment           
+140     Dusky Pigment               
+170     Golden Pigment              
+170     Emerald Pigment             
+100     Violet Pigment              
+170     Silvery Pigment             
+180     Nether Pigment              
+160     Azure Pigment               
+140     Ashen Pigment               
+320     Shadow Pigment      
+
+
+inscription 
+
+140     Alabaster Pigment           Earthroot, Peacebloom, Silverleaf
+140     Dusky Pigment               Briarthorn, Bruiseweed, Mageroyal, Stranglekelp, Swiftthistle
+170     Golden Pigment              Grave Moss, Kingsblood, Liferoot, Wild Steelbloom
+170     Emerald Pigment             Dragon's Teeth, Fadeleaf, Goldthorn, Khadgar's Whisker
+100     Violet Pigment              Arthas' Tears, Blindweed, Firebloom, Ghost Mushroom, Gromsblood, Purple Lotus, Sungrass
+170     Silvery Pigment             Dreamfoil, Golden Sansam, Icecap, Mountain Silversage, Sorrowmoss
+180     Nether Pigment              Ancient Lichen, Dreaming Glory, Felweed, Mana Thistle, Netherbloom, Nightmare Vine, Ragveil, Terocone
+160     Azure Pigment               Adder's Tongue, Deadnettle, Fire Leaf, Goldclover, Icethorn, Lichbloom, Talandra's Rose, Tiger Lily
+140     Ashen Pigment               Cinderbloom , Stormvine ,  Azshara's Veil , Heartblossom , Twilight Jasmine , Whiptail  
+320     Shadow Pigment              Desecrated Herb, Fool's Cap, Green Tea Leaf, Rain Poppy, Silkweed, Snow Lily    
+
+
+
+1208    Frostweave Cloth                            70level         mob drop         Northrend
+905     Embersilk Cloth                             80level         mob drop         Cataclysm                          
+899     thorium bar                                 40level         mined            Winterspring
+830     Netherweave Cloth                           60level         mob drop         Outland
+801     Runecloth                                   50level         mob drop         
+626     mageweave cloth                             40level         mob drop
+621     mithril bar                                 40level         mined            Searing Gorge
+620     Silk Cloth                                  30level         mob drop
+618     ghost iron bar                              86level         mined
+608     cobalt bar                                  72level         mined
+495     Windwool Cloth                              86level         mob drop 
+490     Rugged Leather                              50level         mob drop
+410     Thick Leather
+385     Savage Leather
+385     copper bars
+365     bronze bar
+353     hypnotic dust
+340     arcane dust
+320     Borean Leather
+317     Knothide Leather
+300     Exotic Leather or Prismatic Scale
+284     iron bar
+280     linen cloth 
+260     Light Leather
+241     obsidium bar 
+230     dream dust
+220     solid stone 
+215     heavy stone
+195     Wool Cloth
+195     Heavy Leather
+193     rough stone
+175     Medium Leather
+172     saronite bar 
+160     elementium bar 
+155     vision dust 
+137     spirit dust
+134     strange dust
+125     adamanite bar
+124     crystallized water
+122     infinite dust 
+120     soul dust 
+120     Heavy Borean Leather (720 Borean Leather)
+100     coarse stone 
+102     fel iron bar
+100     Knothide Leather Scraps
+100     grean tea leaf 
+93      briathorn
+86      goldclover
+78      volatile earth
+75      tin bar 
+65      Sha-Touched Leatherette
+60      cinderbloom 
+60      dense stone 
+60      lesser moonstone
+60      rain poppy or silkweed
+60      snow lily
+59      silverleaf
+59      peacebloom
+57      Ruined Leather Scraps
+54      volatile earth 
+54      steel bar 
+50      Savage Leather Scraps
+50      felweed 
+46      golden sansam
+45      stranglekelp 
+45      khadgar's whisker 
+40      gromsblood
+40      Exotic Leather
+40      lichbloom 
+37      tigerlily 
+35      tinker's kit
+35      dreaming glory 
+35      goldthorn 
+34      mysterious essence 
+33      bruiseweed 
+32      volatile air
+30      sorrowmoss 
+30      kingsblood 
+30      greater celestial essence 
+30      Hypnotic Dust
+27      greater cosmic essence 
+26      Fel Scales
+25      adder's tongue 
+25      sungrass
+25      citrine
+25      fool's cap
+25      heartblossom
+25      lesser astral essence
+25      lesser nether essence 
+24      mote of earth
+24      purple lotus
+21      Heavy Savage Leather (110 Savage Leather)
+20      Heavy Hide
+20      huge emerald 
+20      icethorn
+20      large opal 
+20      lesser planar essence 
+20      liferoot
+20      mageroyal 
+20      tigerseye
+20      talandra's rose
+20      small lustrous pearl
+20      Eternal Life or Fire or Shadow
+20      crystalized air
+18      mountain silversage 
+16      azshara's veil 
+15      abyss crystal 
+15      crystal vial 
+15      dreamfoil 
+15      Medium Hide
+15      nightmare vine 
+15      true silver bar
+15      lesser eternal essence 
+12      mote of fire   
+10      crystallized earth 
+10      stormvine 
+10      blindweed 
+10      wild steelbloom
+10      terocone
+10      volatile fire 
+10      simple wood vendor 
+10      netherbloom 
+10      star ruby 
+10      spirit of harmony
+10      greater magic essence 
+10      lesser celestial essence
+7       azerothian diamond                                              mining thorium
+7       silver bar 
+5       aquamarine
+5       Eternal Water
+5       titanium bar 
+5       twilight jasmine 
+5       large prismatic shard                                           level 70 created by disenchanting
+5       volatile life                                                   drops from cataclysm herbs
+4       firebloom                                                       level 41 have
+4       alicite                                                         level 81 Cataclysm ores mining
+4       mos agate                                                       level 25 mined or mob drop dungeon(got prob)                                                   
+3       blue sapphire                                                   level 55 mined form thorium veins
+3       dream chard                                                     level 80 disenchanting level 165+
+3       essence of the undeadth                                         level 55 drop of  Undead mobs in Dire Maul, Eastern Plaguelands, Naxxramas, Scholomance, Stratholme, Western Plaguelands, and Zul'Gurub. ore crafted alchemy 275
+2       Volatile Water                                                  level 83 drop while mining Elementium
+1       black vitriol                                                   level 40 item gem like got it possibly
+1       copper rod                                                      blacksmith creation
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
--- presentation/Home_presentation.php
+++ presentation/Home_presentation.php
@@ -1,8 +1,43 @@
-<?php
+<!DOCTYPE html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html>
+    <head>
+        <title>Descan Mathias Eindtest PHP piza shop</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/Main_Css.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="cotianer">
+            <header class="header">
+                <section class="container_nav_title">
+                    <section class="title">
+                        
+                    </section>
+                    <nav class="headmenu">
+                        <ul class="menu">
+                            <li>home</li>
+                            <li>register</li>
+                        </ul>
+                    </nav>
+                </section>
+                <section class="login">
+                    <form method="post" action="login.php">
+                            <label for="username">username:</label>
+                            <input type="text" name="username" value="" placeholder="username"><br/>
+                            <label for="password">password:</label>
+                            <input type="password" name="password" value=""><br/>
+                            <input type="submit" value="login" name="submit">
+                        </form> 
+                </section>
+            </header>
+            <section class="body">
+                test
+            </section>
+            <footer class="footer">
+                test
+            </footer>
+        </div>
+    </body>
+</html>
 
